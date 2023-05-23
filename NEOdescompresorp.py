f6dc029d3a969c46c73979e889bc4b4fca90f714
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2023-05-23 07:04:43 -0500

    NEOdescompresor

--- NEOdescompresorp.py
+++ NEOdescompresorp.py
@@ -3,6 +3,7 @@ import sys
 import time
 import pickle
 from mpi4py import MPI
+from math import ceil
 
 class HuffmanDecompressor:
 
@@ -68,6 +69,7 @@ class HuffmanDecompressor:
 			decompressed_text = self.decode_text(encoded_text)
 			decompressed_binary = bytes.fromhex(decompressed_text)
 		
+		return decompressed_binary, output_file_extension
 		with open(self.output_path+output_file_extension, 'wb') as output_file:
 			output_file.write(decompressed_binary)
 	
@@ -75,57 +77,51 @@ class HuffmanDecompressor:
 	#	with open('reverse_mapping', 'rb') as reverse_mapping_file:
 	#		self.reverse_mapping = pickle.load(reverse_mapping_file)
 
-def remove_padding(self, padded_encoded_text):
-	padded_info = padded_encoded_text[:8]
-	extra_padding = int(padded_info, 2)
-	padded_encoded_text = padded_encoded_text[8:] 
-	encoded_text = padded_encoded_text[:-1*extra_padding]
-	return encoded_text
+def divide_data(data, size):
+    parts = []
+    chunckSize = ceil(len(data)/(size-1))
+    for i in range(0, size):
+        if(i == size-1):
+            parts.append(data[chunckSize*(i-1):len(data)])
+        else:
+            parts.append(data[chunckSize*(i-1):chunckSize*i])
+    return parts, chunckSize
 
 def main():
 	comm = MPI.COMM_WORLD
-	rank = comm.Get_rank()
 	size = comm.Get_size()
+	rank = comm.Get_rank()
+	amode = MPI.MODE_WRONLY|MPI.MODE_CREATE
+	output_path = "descomprimidop-elmejorprofesor"
+	if rank == 0:
+
+		input_path = sys.argv[1]
+
+		if(os.path.isfile(input_path) == False):
+			print(input_path+" does not exist")
+			exit(0)
+		hd = HuffmanDecompressor(input_path, output_path)
+		st = time.time()
+		decoded_text, output_file_extension = hd.decompress()
+		divided_decoded_text, chunckSize = divide_data(decoded_text, size)
+	else:
+		decoded_text = None
+		output_file_extension = None
+		chunckSize = None
+		divided_decoded_text = None
+	
+	divided_decoded_text = comm.scatter(divided_decoded_text, root = 0)
+	output_file_extension = comm.bcast(output_file_extension, root = 0)
+	chunckSize = comm.bcast(chunckSize, root = 0)
+	fh = MPI.File.Open(comm, output_path+output_file_extension, amode)
+	offset = chunckSize*(rank-1)
+	fh.Write_at_all(offset, divided_decoded_text)
+	fh.Close()
 
 	if rank == 0:
-		st = time.time()
-		input_path = sys.argv[1]
-		output_path = "descomprimidop-elmejorprofesor"
-		with open(input_path, 'rb') as input_file:
-			
-			#Load the reverse_mapping
-			n = int.from_bytes(input_file.read(4), byteorder=sys.byteorder)
-			output_file_extension_as_bytes = input_file.read(n)
-			output_file_extension = output_file_extension_as_bytes.decode()
-
-			#Load the reverse_mapping
-			n = int.from_bytes(input_file.read(4), byteorder=sys.byteorder)
-			reverse_mapping_bytes = input_file.read(n)
-			reverse_mapping = pickle.loads(reverse_mapping_bytes)
-
-			bit_string = ""
-
-			#Load the compressed file
-			byte = input_file.read(1)
-			while(len(byte) > 0):
-				byte = ord(byte)
-				bits = bin(byte)[2:].rjust(8, '0')
-				bit_string += bits
-				byte = input_file.read(1)
-
-			encoded_text = remove_padding(bit_string)
-
-	
-	hd = HuffmanDecompressor(input_path, output_path)
-	#hd.load_reverse_mapping()
-
-
-	
-	hd.decompress()
-	et = time.time()
-	ft = et-st
-	print(ft)
-
+		et = time.time()
+		ft = et-st
+		print(ft)
 
 if __name__ == "__main__":
     main()
